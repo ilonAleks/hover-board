9f878b08de3c949ca89b590823fd17a1599b26e4
ilonAleks <106811359+ilonAleks@users.noreply.github.com>
2022-09-16 19:13:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,22 +6,23 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
 	const square = document.createElement('div')
 	square.classList.add('square')
 
-	square.addEventListener('mouseover', () =>
-		setColor(square))
+	square.addEventListener('mouseover', setColor)
 
-	square.addEventListener('mouseleave', () =>
-		removeColor(square))
+
+	square.addEventListener('mouseleave', removeColor)
 
 	board.append(square)
 }
 
-function setColor(element) {
+function setColor(event) {
+	const element = event.target
 	const color = getRandomColor()
 	element.style.backgroundColor = color
 	element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
 
-function removeColor(element) {
+function removeColor(event) {
+	const element = event.target
 	element.style.backgroundColor = '#1d1d1d'
 	element.style.boxShadow = `0 0 2px #000`
 }
